23da93d7eaf6aec8c65f65446231542e32fd220c
AMD <michojekunle1@gmail.com>
2024-09-28 12:39:49 +0100

    changes

--- quiz-app-react/src/App.tsx
+++ quiz-app-react/src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import QuizPage from './pages/QuizPage';
-import ResultsPage from './pages/ResultsPage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import QuizPage from "./pages/QuizPage";
+import ResultsPage from "./pages/ResultsPage";
 
 const App: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/quiz" element={<QuizPage />} />
-      <Route path="/results" element={<ResultsPage />} />
+      <section className="px-6">
+        <Route path="/" element={<HomePage />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/results" element={<ResultsPage />} />
+      </section>
     </Routes>
   );
 };
