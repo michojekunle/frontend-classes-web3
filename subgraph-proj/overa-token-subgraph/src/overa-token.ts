d0b4b9c4296deba92ffd51c6f662afd61d525740
AMD <michojekunle1@gmail.com>
2024-10-05 08:43:11 +0100

    more complex relations

--- subgraph-proj/overa-token-subgraph/src/overa-token.ts
+++ subgraph-proj/overa-token-subgraph/src/overa-token.ts
@@ -6,7 +6,15 @@ import {
   Transfer as TransferEvent,
   Approval as ApprovalEvent,
 } from "../generated/OveraToken/OveraToken";
-import { Burn, Approval, Transfer, User, Token, Mint, OwnershipTransferred } from "../generated/schema";
+import {
+  Burn,
+  Approval,
+  Transfer,
+  User,
+  Token,
+  Mint,
+  OwnershipTransferred,
+} from "../generated/schema";
 
 export function handleApproval(event: ApprovalEvent): void {
   let approval = new Approval(
@@ -17,12 +25,24 @@ export function handleApproval(event: ApprovalEvent): void {
   approval.value = event.params.value;
   approval.timestamp = event.block.timestamp;
 
+  let user = User.load(event.params.owner.toHex());
+  if (user == null) {
+    user = new User(event.params.owner.toHex());
+    user.balance = BigInt.fromI32(0);
+    user.token = event.address.toString();
+  }
+
+  approval.user = user.id;
   approval.save();
 }
 
-export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void {
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
   // create a new ownership transferred entity
-  let ownershipTransferred  = new OwnershipTransferred(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
+  let ownershipTransferred = new OwnershipTransferred(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
 
   ownershipTransferred.newOwner = event.params.newOwner;
   ownershipTransferred.previousOwner = event.params.previousOwner;
@@ -41,13 +61,15 @@ export function handleTokensBurned(event: TokensBurned): void {
   burn.from = event.params.from;
   burn.value = event.params.amount;
   burn.timestamp = event.block.timestamp;
+  burn.user = event.params.from.toString();
   burn.save();
 
   // update Sender balances
   let fromUser = User.load(event.params.from.toHex());
-  if(fromUser == null) {
+  if (fromUser == null) {
     fromUser = new User(event.params.from.toHex());
     fromUser.balance = BigInt.fromI32(0);
+    fromUser.token = event.address.toString();
   }
 
   fromUser.balance = fromUser.balance.minus(event.params.amount);
@@ -55,7 +77,7 @@ export function handleTokensBurned(event: TokensBurned): void {
 
   // update the total supply and total burned in tokens entity
   let token = Token.load("Token Stats");
-  if(token === null){
+  if (token === null) {
     token = new Token("Token Stats");
     token.totalSupply = BigInt.fromI32(0);
     token.totalBurned = BigInt.fromI32(0);
@@ -69,83 +91,88 @@ export function handleTokensBurned(event: TokensBurned): void {
 }
 
 export function handleTokensMinted(event: TokensMinted): void {
-    // create a new Entity
-    let mint = new Mint(
-      event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-    );
-  
-    mint.to = event.params.to;
-    mint.value = event.params.amount;
-    mint.timestamp = event.block.timestamp;
-    mint.save();
-  
-    // update reciever balances
-    let toUser = User.load(event.params.to.toHex());
-    if(toUser == null) {
-      toUser = new User(event.params.to.toHex());
-      toUser.balance = BigInt.fromI32(0);
-    }
-  
-    toUser.balance = toUser.balance.plus(event.params.amount);
-    toUser.save();
-  
-    // update the total supply and total minted in tokens entity
-    let token = Token.load("Token Stats");
-    if(token === null){
-      token = new Token("Token Stats");
-      token.totalSupply = BigInt.fromI32(0);
-      token.totalBurned = BigInt.fromI32(0);
-      token.totalMinted = BigInt.fromI32(0);
-      token.totalTranferred = BigInt.fromI32(0);
-    }
-  
-    token.totalSupply = token.totalSupply.plus(event.params.amount);
-    token.totalMinted = token.totalMinted.plus(event.params.amount);
-    token.save();
+  // create a new Entity
+  let mint = new Mint(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+
+  mint.to = event.params.to;
+  mint.value = event.params.amount;
+  mint.timestamp = event.block.timestamp;
+  mint.user = event.params.to.toString();
+  mint.save();
+
+  // update reciever balances
+  let toUser = User.load(event.params.to.toHex());
+  if (toUser == null) {
+    toUser = new User(event.params.to.toHex());
+    toUser.balance = BigInt.fromI32(0);
+    toUser.token = event.address.toString();
+  }
+
+  toUser.balance = toUser.balance.plus(event.params.amount);
+  toUser.save();
+
+  // update the total supply and total minted in tokens entity
+  let token = Token.load("Token Stats");
+  if (token === null) {
+    token = new Token("Token Stats");
+    token.totalSupply = BigInt.fromI32(0);
+    token.totalBurned = BigInt.fromI32(0);
+    token.totalMinted = BigInt.fromI32(0);
+    token.totalTranferred = BigInt.fromI32(0);
+  }
+
+  token.totalSupply = token.totalSupply.plus(event.params.amount);
+  token.totalMinted = token.totalMinted.plus(event.params.amount);
+  token.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
-    // create a new Entity
-    let transfer = new Transfer(
-      event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-    );
-  
-    transfer.from = event.params.from;
-    transfer.to = event.params.to;
-    transfer.value = event.params.value;
-    transfer.timestamp = event.block.timestamp;
-    transfer.save();
-  
-    // update sender balances
-    let fromUser = User.load(event.params.from.toHex());
-    if(fromUser == null) {
-      fromUser = new User(event.params.from.toHex());
-      fromUser.balance = BigInt.fromI32(0);
-    }
-  
-    fromUser.balance = fromUser.balance.minus(event.params.value);
-    fromUser.save();
+  // create a new Entity
+  let transfer = new Transfer(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
 
-    // update receiver balances
-    let toUser = User.load(event.params.to.toHex());
-    if(toUser == null) {
-      toUser = new User(event.params.to.toHex());
-      toUser.balance = BigInt.fromI32(0);
-    }
-  
-    toUser.balance = toUser.balance.plus(event.params.value);
-    toUser.save();
-  
-    // update the total transferred in tokens entity
-    let token = Token.load("Token Stats");
-    if(token === null){
-      token = new Token("Token Stats");
-      token.totalSupply = BigInt.fromI32(0);
-      token.totalBurned = BigInt.fromI32(0);
-      token.totalMinted = BigInt.fromI32(0);
-      token.totalTranferred = BigInt.fromI32(0);
-    }
-    
-    token.totalTranferred = token.totalTranferred.plus(event.params.value);
-    token.save();
+  transfer.from = event.params.from;
+  transfer.to = event.params.to;
+  transfer.value = event.params.value;
+  transfer.timestamp = event.block.timestamp;
+  transfer.user = event.params.from.toString();
+  transfer.save();
+
+  // update sender balances
+  let fromUser = User.load(event.params.from.toHex());
+  if (fromUser == null) {
+    fromUser = new User(event.params.from.toHex());
+    fromUser.balance = BigInt.fromI32(0);
+    fromUser.token = event.address.toString();
+  }
+
+  fromUser.balance = fromUser.balance.minus(event.params.value);
+  fromUser.save();
+
+  // update receiver balances
+  let toUser = User.load(event.params.to.toHex());
+  if (toUser == null) {
+    toUser = new User(event.params.to.toHex());
+    toUser.balance = BigInt.fromI32(0);
+    toUser.token = event.address.toString();
+  }
+
+  toUser.balance = toUser.balance.plus(event.params.value);
+  toUser.save();
+
+  // update the total transferred in tokens entity
+  let token = Token.load("Token Stats");
+  if (token === null) {
+    token = new Token("Token Stats");
+    token.totalSupply = BigInt.fromI32(0);
+    token.totalBurned = BigInt.fromI32(0);
+    token.totalMinted = BigInt.fromI32(0);
+    token.totalTranferred = BigInt.fromI32(0);
+  }
+
+  token.totalTranferred = token.totalTranferred.plus(event.params.value);
+  token.save();
 }
